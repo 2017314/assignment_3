05cfb3af69d01a35146645107287a1a0ea200bd8
Sameed <73094942+2017314@users.noreply.github.com>
2020-11-06 10:52:57 +0500

    Update App.js

--- new/src/App.js
+++ new/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi this is 2017314 ok boy
+          Hi this is 2017314
         </p>
         <a
           className="App-link"
